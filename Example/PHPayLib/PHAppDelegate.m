5e7c7f54e23589690c4fefa59caf4f8238718112
相公 <xianggong@anve.com>
2018-07-07 10:17:22 +0800

    no message

--- Example/PHPayLib/PHAppDelegate.m
+++ Example/PHPayLib/PHAppDelegate.m
@@ -12,6 +12,7 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
+    NSLog(@"111111");
     // Override point for customization after application launch.
     return YES;
 }
